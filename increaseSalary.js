bed223a9e0b7bf7ba7cf1fae7ee270b3c9e501d3
Alexei Okhapkin <alexeyyago@gmail.com>
2023-12-17 17:04:12 +0300

    async function increaseSalary

--- increaseSalary.js
+++ increaseSalary.js
@@ -1,25 +1,61 @@
 
-function increaseSalary() {
-  // Write your code here
-  console.log("here")
-  return api.getEmployees()
-  .then(employeesArray => {
-    const salaries = employeesArray.map(item => item.salary);
-    const minSalary = Math.min(...salaries);
-    const minSalaryEmployee = employeesArray.find(item => item.salary === minSalary);
-    const newSalary = minSalary * 1.2;
+// function increaseSalary() {
+//   // Write your code here
+//   console.log("here")
+//   return api.getEmployees()
+//   .then(employeesArray => {
+//     const salaries = employeesArray.map(item => item.salary);
+//     const minSalary = Math.min(...salaries);
+//     const minSalaryEmployee = employeesArray.find(item => item.salary === minSalary);
+//     const newSalary = minSalary * 1.2;
 
-    console.log(newSalary)
+//     console.log(newSalary)
 
-    return api.setEmployeeSalary(minSalaryEmployee.id, newSalary);
-  })
-  .then(employee => {
-    console.log(employee);
-    api.notifyEmployee(employee.id, `Hello, ${employee.name}! Congratulations, your new salary is ${employee.salary}!`);
-  })
-  .catch(e => {
+//     return api.setEmployeeSalary(minSalaryEmployee.id, newSalary);
+//   })
+//   .then(employee => {
+//     console.log(employee);
+//     api.notifyEmployee(employee.id, `Hello, ${employee.name}! Congratulations, your new salary is ${employee.salary}!`);
+//   })
+//   .catch(e => {
+//     api.notifyAdmin(e);
+//   });
+// }
+
+// async/await
+
+async function increaseSalary() {
+  try {
+    const employees = await api.getEmployees();
+    const allSalaries = employees.reduce((accum, item) => accum + item.salary, 0);
+    const arithmeticMeanSalary = allSalaries / employees.length;
+    let counter = 0;
+    let totalSalaries = 0;
+
+    for (let employee of employees) {
+      let newSalary = 0;
+      if (employee.salary < arithmeticMeanSalary) {
+        newSalary = employee.salary * 1.2;
+      } else {
+        newSalary = employee.salary * 1.1;
+      }
+
+      try {
+        employee = await api.setEmployeeSalary(employee.id, newSalary);
+        counter++;
+        totalSalaries += employee.salary;
+        await api.notifyEmployee(employee.id, `Hello, ${employee.name}! Congratulations, your new salary is ${employee.salary}!`);
+      } catch (e) {
+        await api.notifyAdmin(e)
+      }
+    }
+
+    await api.sendBudgetToAccounting(totalSalaries);
+
+    return counter;
+  } catch (e) {
     api.notifyAdmin(e);
-  });
+  }
 }
 
 const api = {
